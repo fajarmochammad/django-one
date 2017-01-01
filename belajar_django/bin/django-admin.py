7b54122ab6d2b346d856ac8c67e78019e5017c9a
Fajar Moch <fajarmochammad@gmail.com>
2017-01-01 16:05:57 +0700

    First Django first commit

---
+++ belajar_django/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/pongs/Documents/Ubuntubackup/py_file/Projects/belajar_django/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
